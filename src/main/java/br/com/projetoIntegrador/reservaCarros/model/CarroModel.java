36b30a37c1dcdfaf9a1b4388217f258e91916268
is-isadora-rocha <is.isadora.rocha@gmail.com>
2023-10-20 11:55:12 -0300

    configurações do banco

--- src/main/java/br/com/projetoIntegrador/reservaCarros/model/CarroModel.java
+++ src/main/java/br/com/projetoIntegrador/reservaCarros/model/CarroModel.java
@@ -3,7 +3,6 @@ package br.com.projetoIntegrador.reservaCarros.model;
 import java.util.Date;
 import java.util.UUID;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
